2f9527613908c87615efe9698c7f43dba76e2901
Cheng Kuan <ck@codeck.org>
2016-06-27 16:02:36 +0800

    added script to corrupt history archive for testing

---
+++ tests/corrupt_archive.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm archive/ledger/00/01/02/ledger-0001023f.xdr.gz
+cat /dev/null >archive/ledger/00/02/01/ledger-000201ff.xdr.gz
+cat /dev/null >archive/history/00/02/01/history-000201ff.json
+
